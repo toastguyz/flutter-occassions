26aed1db547d72a34194e3f72c916b98a3fcabc7
toastguyz <toastguyz.github@gmail.com>
2019-10-05 15:49:35 +0530

    Readme.md file and changes updated.

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Explore',
+      title: 'Flutter Occassions',
       theme: ThemeData(
           brightness: Brightness.light,
           cursorColor: activeColor,
